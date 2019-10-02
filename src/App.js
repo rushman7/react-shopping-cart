ae2fd2dc4a93183602a9bed739684f2a9909e84b
BuildTools <arthurpisakhov1@gmail.com>
2019-10-01 20:35:06 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
@@ -15,14 +16,18 @@ function App() {
 	};
 
 	return (
-		<ProductContext.Provider value={{ products, addItem, cart }}>
-			<div className="App">
-				<Navigation cart={cart} />
-				<Route exact path="/" component={Products}/>
+		<div className="App">
 
+			<CartContext.Provider value={{ cart }}>
+				<Navigation cart={cart} />
 				<Route path="/cart" component={ShoppingCart}/>
-			</div>
-		</ProductContext.Provider>
+			</CartContext.Provider>
+
+			<ProductContext.Provider value={{ products, addItem }}>
+				<Route exact path="/" component={Products}/>
+			</ProductContext.Provider>
+
+		</div>
 	);
 }
 
